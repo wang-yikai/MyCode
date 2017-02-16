d2ce720bd15488df687435195389ac0548fa71c2
getDolla <ywang8@stuy.edu>
2017-02-16 14:51:58 -0500

    updated notes

--- animation.js
+++ animation.js
@@ -6,8 +6,27 @@ var xcor, ycor;
 var lastx = -1;
 var lasty = -1;
 
-/* start drawing lines */
+var x = 40;
+var rid = 0;
+
+/* start drawing */
 ctx.beginPath();
+
+var drawCircle = function(evt) {
+   /* gets random hex by converting Math.random() into a hex string keeping only the values from index 2 to 7
+     (via slice), convert to lowercase, and keeping only the last 6 elements of the resulting sequence */
+  ctx.fillStyle = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toLowerCase()).slice(-6);
+  ctx.arc(x, ycor, 20, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
+  ctx.beginPath();
+  x++;
+  rid = window.requestAnimationFrame( drawCircle );
+};
+
+var stopIt = function() {
+    window.cancelAnimationFrame( rid );
+};
 
 /* keeps track of the xcor and ycor of mouse */
 canvas.addEventListener("mousemove", function(evt) {
@@ -15,18 +34,11 @@ canvas.addEventListener("mousemove", function(evt) {
     ycor = evt.offsetY;
 });
 
-canvas.addEventListener('click', function(evt) {
-   /* gets random hex by converting Math.random() into a hex string keeping only the values from index 2 to 7
-     (via slice), convert to lowercase, and keeping only the last 6 elements of the resulting sequence */
-  ctx.fillStyle = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toLowerCase()).slice(-6);
-  ctx.arc(xcor, ycor, 20, 0, 2 * Math.PI);
-  ctx.fill();
-  ctx.stroke();
-  ctx.beginPath();
-});
+canvas.addEventListener('click', drawCircle );
 
 
 button.addEventListener("click", function(evt) {
+    stopIt;
     ctx.clearRect(0,0, canvas.width, canvas.height);
     ctx.beginPath();
     lastx = -1;
