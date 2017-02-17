80298236998760f07f0632e62add08b4698b36ce
Yikai <ywang8@stuy.edu>
2017-02-17 14:27:11 -0500

    updated animations

--- animation.js
+++ animation.js
@@ -3,28 +3,36 @@ var ctx = canvas.getContext('2d');
 var button = document.getElementById("button");
 var xcor, ycor;
 
-var x = 40;
 var rid = 0;
 
 /* start drawing */
 ctx.beginPath();
 
-var drawCircle = function(evt) {
-  ctx.clearRect(0,0, canvas.width, canvas.height);
-
-   /* gets random hex by converting Math.random() into a hex string keeping only the values from index 2 to 7
-     (via slice), convert to lowercase, and keeping only the last 6 elements of the resulting sequence */
-  ctx.fillStyle = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toLowerCase()).slice(-6);
-  ctx.arc(x, canvas.height/2, 20, 0, 2 * Math.PI);
-  ctx.fill();
-  ctx.stroke();
-  ctx.beginPath();
-  x++;
-  rid = window.requestAnimationFrame( drawCircle );
-};
 
 var stopIt = function() {
     window.cancelAnimationFrame( rid );
+};
+
+/* closure structure */
+var animate = function(evt) {
+  var x = 40;
+  stopIt();
+
+  var drawCircle = function(evt) {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+
+     /* gets random hex by converting Math.random() into a hex string keeping only the values from index 2 to 7
+       (via slice), convert to lowercase, and keeping only the last 6 elements of the resulting sequence */
+    ctx.fillStyle = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toLowerCase()).slice(-6);
+    ctx.arc(x, canvas.height/2, 20, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    ctx.beginPath();
+    x++;
+    rid = window.requestAnimationFrame( drawCircle );
+  };
+
+  drawCircle();
 };
 
 // /* keeps track of the xcor and ycor of mouse */
@@ -33,7 +41,7 @@ var stopIt = function() {
 //     ycor = evt.offsetY;
 // });
 
-canvas.addEventListener('click', drawCircle );
+canvas.addEventListener('click', animate );
 
 
 button.addEventListener("click", function(evt) {
