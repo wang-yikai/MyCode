f167b0ff4dfc1f18f92f85a9e7df0d3176e9a700
Yikai <calvin0987@gmail.com>
2021-09-30 22:42:29 -0400

    Update globals.py

--- globals.py
+++ globals.py
@@ -17,3 +17,22 @@ def not_reset():
 reset()
 print c
 
+def gcd(x, y): 
+   while y: 
+       x, y = y, x % y 
+   return x 
+
+def nthUglyNumber(n: int, a: int, b: int) -> int:
+        low = 1
+        high = 2 * 10 ** 9
+        ab = (a * b) / gcd(a, b)
+        while low < high:
+            mid = (low + high) / 2
+            count = mid / a + mid / b - mid / ab
+            if (count < n):
+                low = mid + 1
+            else:
+                high = mid
+        return int(low)
+
+print(nthUglyNumber(5, 2, 4))
